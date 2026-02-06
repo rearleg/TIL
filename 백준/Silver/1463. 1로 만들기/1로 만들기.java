15f4548ed26c053e74ab2313455d3e8a447ef7d7
rearleg <frameonmoment@gmail.com>
2026-02-06 12:06:50 +0900

    [Silver III] Title: 1로 만들기, Time: 112 ms, Memory: 18028 KB -SsafyToday

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.io.*;
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws Exception {
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] dp = new int[n+1];
+		
+		for (int i = 2; i <= n; i++) {
+			
+			dp[i] = dp[i-1]+1;
+			
+			if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i/2]+1);
+			if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+			
+		}
+		
+		System.out.println(dp[n]);
+		
+	}	
+		
+}
