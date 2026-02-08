12c6709604319401b6b9a10480f9860a6f5b4c05
rearleg <frameonmoment@gmail.com>
2026-02-08 21:20:05 +0900

    [Platinum V] Title: 찾기, Time: 536 ms, Memory: 75584 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,23 +1,20 @@
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 import java.util.stream.Collectors;
-
 
 public class Main {
     static int cnt = 0;
     static List<Integer> matchIdx;
 
-    public static void main(String[] args) throws Exception {
-        // 초기화
+    public static void main(String[] args) throws IOException {
         matchIdx = new ArrayList<>();
 
-        // 입력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String t = br.readLine();
+        String s = br.readLine();
         String p = br.readLine();
 
-        kmp(t, p);
+        kmp(s, p);
 
         System.out.println(cnt);
         System.out.println(matchIdx.stream().map(String::valueOf).collect(Collectors.joining(" ")));
@@ -29,34 +26,23 @@ public class Main {
         int[] pi = new int[n];
 
         for (int i = 1, j = 0; i < n; i++) {
-            while (j > 0 && p.charAt(i) != p.charAt(j)) {
-                j = pi[j - 1];
-            }
+            while (j > 0 && p.charAt(i) != p.charAt(j)) j = pi[j - 1];
             if (p.charAt(i) == p.charAt(j)) pi[i] = ++j;
         }
+
         return pi;
     }
 
-    static boolean kmp(String t, String p) {
-        int n = t.length();
+    static boolean kmp(String s, String p) {
+        int n = s.length();
         int m = p.length();
-
-        if (m == 0) return true;
-
         int[] pi = buildPi(p);
 
         for (int i = 0, j = 0; i < n; i++) {
-
-            // 불일치
-            while (j > 0 && t.charAt(i) != p.charAt(j)) {
-                j = pi[j - 1];
-            }
-
-            // 일치
-            if (t.charAt(i) == p.charAt(j)) {
+            while(j > 0 && s.charAt(i) != p.charAt(j)) j = pi[j - 1];
+            if (s.charAt(i) == p.charAt(j)) {
                 if (j == m - 1) {
                     cnt++;
-                    // 시작 인덱스 반환, i - (m - 1) + 1
                     matchIdx.add(i - m + 2);
                     j = pi[j];
                     continue;
@@ -66,5 +52,4 @@ public class Main {
         }
         return false;
     }
-
 }
