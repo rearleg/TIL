0507ab2e453fe5a2ba0f2cfc5018303e33869eff
Cheon Changhyeon <frameonmoment@gmail.com>
2026-02-13 00:03:51 +0900

    [Bronze IV] Title: 입실 관리, Time: 108 ms, Memory: 14196 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine().trim());
+        for (int tc = 0; tc < T; tc++) {
+            String s = br.readLine().toLowerCase();
+            System.out.println(s);
+
+        }
+    }
+}
