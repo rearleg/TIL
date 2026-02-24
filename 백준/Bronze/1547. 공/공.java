9792ed4ef5266d6c4734d857c93b9b56e39d685e
Cheon Changhyeon <frameonmoment@gmail.com>
2026-02-24 23:53:50 +0900

    [Bronze III] Title: 공, Time: 104 ms, Memory: 14268 KB -SsafyToday

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.util.stream.Collectors;
+
+class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int ball = 1;
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a == ball) ball = b;
+            else if (b == ball) ball = a;
+        }
+        System.out.println(ball);
+    }
+
+}
