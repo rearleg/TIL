736b41f810171c48d622e8f63f86f5f9cf65dfa9
Cheon Changhyeon <frameonmoment@gmail.com>
2026-03-10 14:32:18 +0900

    [Gold IV] Title: 부분합, Time: 244 ms, Memory: 24236 KB -SsafyToday

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        int l = 0, r = 0;
+        int sum = 0;
+        int ans = Integer.MAX_VALUE;
+
+        while (true) {
+            if (sum >= M) {
+                ans = Math.min(ans, r - l);
+                sum -= arr[l++];
+            } else if (r == N) break;
+            else sum += arr[r++];
+        }
+
+        System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
+    }
+}
+
