cfef90c25934eb55f5dd1a79b83592ecd22ed91e
Cheon Changhyeon <frameonmoment@gmail.com>
2026-03-10 09:20:30 +0900

    [Silver IV] Title: 문자열 집합, Time: 2320 ms, Memory: 873400 KB -SsafyToday

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -2,21 +2,61 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    static class Node {
+        Node[] child = new Node[26];
+        boolean end;
+    }
+
+    static Node root = new Node();
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        Set<String> set = new HashSet<>();
-        for (int i = 0 ; i < N; i++) set.add(br.readLine().trim());
+        for (int i = 0; i < N; i++) {
+            insert(br.readLine());
+        }
 
         int cnt = 0;
-        for (int i = 0 ; i < M; i++) {
-            if (set.contains(br.readLine().trim())) cnt++;
 
+        for (int i = 0; i < M; i++) {
+            if (search((br.readLine()))) {
+                cnt++;
+            }
         }
         System.out.println(cnt);
+
     }
+
+    static boolean search(String s) {
+        Node cur = root;
+
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+
+            if (cur.child[idx] == null) {
+                return false;
+            }
+            cur = cur.child[idx];
+        }
+        return cur.end;
+    }
+
+    static void insert(String s) {
+        Node cur = root;
+
+        for (int i = 0; i< s.length(); i++) {
+            int idx = s.charAt(i) - 'a';
+
+            if (cur.child[idx] == null) {
+                cur.child[idx] = new Node();
+            }
+            cur = cur.child[idx];
+        }
+        cur.end = true;
+    }
+
 }
 
