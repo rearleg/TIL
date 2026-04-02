9d344266cc548ed9e9485ee37d01280f3d2695fe
Cheon Changhyeon <frameonmoment@gmail.com>
2026-04-03 00:36:29 +0900

    [Bronze V] Title: CASIO, Time: 108 ms, Memory: 14116 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        
+        if (a == b) System.out.println(1);
+        else System.out.println(0);
+    }
+}
