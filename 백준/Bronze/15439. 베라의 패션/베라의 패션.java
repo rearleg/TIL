9e9b14eb7bb558d0dd52bd906f2e6e92efa0f5bc
Cheon Changhyeon <frameonmoment@gmail.com>
2026-04-13 00:44:18 +0900

    [Bronze IV] Title: 베라의 패션, Time: 96 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(N * (N - 1));
+    }
+}
