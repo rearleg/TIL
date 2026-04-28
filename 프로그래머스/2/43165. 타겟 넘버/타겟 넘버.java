823d4014602a59b0316e85fe9574cc535d8aa5f2
Cheon Changhyeon <frameonmoment@gmail.com>
2026-04-28 10:12:26 +0900

    [level 2] Title: 타겟 넘버, Time: 58.20 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,36 @@
-class Solution {
-    int answer;
-    void dfs (int index, int sum, int[] numbers, int target) {
-        // 1. 탈출 조건
-        if (index == numbers.length) {
-            if (sum == target) {
-                answer++;
+public class Solution {
+    static int[] op; //연산자(+, -) 저장 배열
+    static int cnt; //방법의 수
+
+    public int solution(int[] numbers, int target) {
+        op = new int[numbers.length];
+        cnt = 0;
+
+        dfs(0, target, numbers);
+
+        int answer = cnt;
+        return answer;
+    }
+
+    public static void dfs(int depth, int target, int[] numbers){
+        if(depth == op.length){
+            int sum = 0;
+            for(int i = 0; i < op.length; i++){
+                int idx = op[i];
+                int num = numbers[i];
+                if(idx == 0){ //0 -> +
+                    sum += num;
+                } else if(idx == 1){ //1 -> -
+                    sum -= num;
+                }
             }
+            if(sum == target) cnt++;
             return;
         }
-        // 2. 수행동작
 
-        dfs(index + 1, sum + numbers[index], numbers, target);
-        dfs(index + 1, sum - numbers[index], numbers, target);
-    }
-    public int solution(int[] numbers, int target) {
-        answer = 0;
-
-        dfs(0,0, numbers, target);
-
-        return answer;
-
+        for(int i = 0; i < 2; i++){
+            op[depth] = i;
+            dfs(depth+1, target, numbers);
+        }
     }
 }
