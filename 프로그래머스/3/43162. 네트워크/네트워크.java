3e0b243f7e3960a5b090b950df094a257d3483d6
Cheon Changhyeon <frameonmoment@gmail.com>
2026-04-29 13:43:20 +0900

    [level 3] Title: 네트워크, Time: 0.32 ms, Memory: 91.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,34 +1,36 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Solution {
-        public static void bfs(int s, int n, int[][] computers, boolean[] visited) {
-        Queue<Integer> q = new ArrayDeque<>();
-        visited[s] = true;
-        q.add(s);
+    public int solution(int n, int[][] computers) {
+        boolean[] connected = new boolean[n];
+        int network = 0;
 
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            for (int next = 0; next < n; next++) {
-                if (computers[cur][next] == 1 && !visited[next]) {
-                    visited[next] = true;
-                    q.add(next);
+        for(int i = 0; i < n; i++){
+            if(!connected[i]){
+                connected[i] = true;
+                bfs(i, n, computers, connected);
+                network++;
+            }
+        }
+
+        int answer = network;
+        return answer;
+    }
+
+    public static void bfs(int computer, int n, int[][] computers, boolean[] connected){
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(computer);
+
+        while(!queue.isEmpty()){
+            int now = queue.poll();
+
+            for(int i = 0; i < n; i++){
+                if(computers[now][i] != 0 && !connected[i]){
+                    connected[i] = true;
+                    queue.add(i);
                 }
             }
         }
-
-    }
-
-    public static int solution(int n, int[][] computers) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                bfs(i, n, computers, visited);
-                answer++;
-            }
-        }
-
-        return answer;
     }
 }
