010d74ee615f112b132abaad3772f35774b4281b
Cheon Changhyeon <frameonmoment@gmail.com>
2026-06-23 11:35:31 +0900

    [level 5] Title: 시험장 나누기, Time: 18.82 ms, Memory: 63.3 MB -BaekjoonHub

--- 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
+++ 프로그래머스/5/81305. 시험장 나누기/시험장 나누기.java
@@ -5,24 +5,24 @@
 import java.util.*;
 
 class Solution {
+    
     static int cut;
     static int limit;
     
     public int solution(int k, int[] num, int[][] links) {
-        
         int n = num.length;
+        
+        int root = findRoot(n, links);
         
         int left = 0;
         int right = 0;
-        
-        int root = findRoot(n, links);
         
         for (int x : num) {
             left = Math.max(left, x);
             right += x;
         }
         
-        int answer = right; // 최대값으로 초기화
+        int answer = right;
         
         while (left <= right) {
             int mid = (left + right) / 2;
@@ -33,12 +33,14 @@ class Solution {
             } else {
                 left = mid + 1;
             }
+            
         }
+        
         
         return answer;
     }
     
-    static boolean canDivide(int root, int mid, int k, int[] num, int[][] links){
+    static boolean canDivide(int root, int mid, int k, int[] num, int[][] links) {
         cut = 0;
         limit = mid;
         
@@ -48,6 +50,7 @@ class Solution {
     }
     
     static int dfs(int cur, int[] num, int[][] links) {
+        
         if (cur == -1) return 0;
         
         int leftChild = links[cur][0];
@@ -56,14 +59,13 @@ class Solution {
         int leftSum = dfs(leftChild, num, links);
         int rightSum = dfs(rightChild, num, links);
         
-        int total = num[cur] + leftSum + rightSum;
-        
-        // 하나도 안 끊어도 되는 경우
-        if (total <= limit) {
-            return total;
+        // 안 끊어도 되는지?
+        if (num[cur] + leftSum + rightSum <= limit) {
+            return num[cur] + leftSum + rightSum;
         }
         
-        // 둘 중 하나를 끊어야 하는 경우 (작은쪽이 무조건 유리 greedy)
+        // 하나만 끊으면 되는가? (끊으면 작은 거)
+        
         int smaller = Math.min(leftSum, rightSum);
         
         if (num[cur] + smaller <= limit) {
@@ -71,15 +73,16 @@ class Solution {
             return num[cur] + smaller;
         }
         
-        // 둘다 끊으면 루트만 보냄
+        // 둘 다 끊어야함?
+        
         cut += 2;
         return num[cur];
     }
     
-    static int findRoot(int n, int[][] links ){
+    static int findRoot(int n, int[][] links) {
         boolean[] isChild = new boolean[n];
         
-        for (int i = 0; i < n; i++ ){
+        for (int i = 0; i < n; i++) {
             int left = links[i][0];
             int right = links[i][1];
             
@@ -90,7 +93,6 @@ class Solution {
         for (int i = 0; i < n; i++ ){
             if (!isChild[i]) return i;
         }
-        
         return -1;
     }
 }
